ec12d722b3c7159daabe87c5e01812363bdc535e
kenji198834 <120023006+kenji198834@users.noreply.github.com>
2022-12-27 11:06:16 +0900

    Issue4の修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -15,7 +15,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:calendars).permit(:date, :plan)
+    params.permit(:date, :plan)
   end
 
   def getWeek
@@ -34,7 +34,11 @@ class CalendarsController < ApplicationController
       plans.each do |plan|
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
+<<<<<<< HEAD
       days = { :month => (@todays_date + x).month, :date => (@todays_date+x).day, :plans => today_plans}
+=======
+      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans}
+>>>>>>> parent of 25b551a (Issue6)
       @week_days.push(days)
     end
 
